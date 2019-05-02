9d61f83abaf1d154ed0d256f3644de1bd453e62b
Pritam Shrestha <shresthapritam7@gmail.com>
2019-05-02 20:12:23 +0545

    Wrap BurgerBuilder Component with withErrorHandler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 
 const INGREDIENTS_PRICE = {
@@ -160,4 +161,4 @@ class BurgerBuilder extends Component{
     }        
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) =>{
+    return class extends Component{
+
+        state = {
+            error: null
+        }
+
+        componentDidMount(){
+
+            axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+
+            });
+            axios.interceptors.response.use(res => res, error =>{ 
+                this.setState({error: error})
+                //console.log(error);
+            });
+        }
+
+        errorConfirmedHandler = () =>{
+            this.setState({error: null})
+        }
+
+        render(){
+            return(
+                <>
+                <Modal 
+                    show={this.state.error}
+                    cancel={this.errorConfirmedHandler}>
+                    {this.state.error ? this.state.error.message :null}
+
+                </Modal>
+                <WrappedComponent {...this.props}/>
+            </>
+            );
+        }
+    }   
+}
+
+export default withErrorHandler;
